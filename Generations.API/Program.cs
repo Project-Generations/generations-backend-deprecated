58b16f7458d7200a6d562ddba18c43c133fd6b46
vlben <ben@hollin.nl>
2023-09-27 14:32:09 +0200

    Created the models

--- Generations.API/Program.cs
+++ Generations.API/Program.cs
@@ -1,11 +1,20 @@
+using Generations.DA;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Enforce lowercase urls
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
 
 var app = builder.Build();
 
--- Generations.DA/DataContext.cs
+++ Generations.DA/DataContext.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Generations.DA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Generations.DA
 {
     public class DataContext : DbContext
     {
-        public DataContext()
-        {
+        protected IConfiguration Configuration { get; set; }
 
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pokemon>().ToTable("pokemons");
+            modelBuilder.Entity<CreatedPokemon>().ToTable("created_pokemons");
+            modelBuilder.Entity<Item>().ToTable("items");
+            modelBuilder.Entity<Move>().ToTable("moves");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<CreatedPokemon> CreatedPokemons { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Move> Moves { get; set; }
     }
 }
